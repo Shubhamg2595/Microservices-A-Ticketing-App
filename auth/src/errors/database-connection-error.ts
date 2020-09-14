b68cb6c4347e1875c12b4a66beb6eb7c6d6a5952
Shubham Gupta <shubhamg2595@gmail.com>
2020-09-14 15:57:07 +0530

    updating subclasses to handle error response

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,10 +1,15 @@
 import { ValidationError } from "express-validator";
 
 export class DatabaseConnectionError extends Error {
+  statusCode = 500;
   reason = "Error connecting to Database";
   constructor() {
     super();
     // Only because we are extending a build-in class
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [{ message: this.reason }];
   }
 }
