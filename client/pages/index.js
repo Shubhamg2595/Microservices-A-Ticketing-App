763e49d5996c84a890510a84e7790218de12f80f
Shubham Gupta <shubhamg2595@gmail.com>
2021-01-27 01:12:48 +0530

    SSR app completed for auth

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,15 @@
 import buildClient from "../api/build-client";
 const LandingPage = ({ currentUser }) => {
-  console.log("currentUser::", currentUser);
-  return <h1>LandingPage</h1>;
+  return currentUser ? (
+    <h1> you are signed in </h1>
+  ) : (
+    <h1> you are not signed in </h1>
+  );
 };
 
 LandingPage.getInitialProps = async (context) => {
+  console.log("inside LandingPgge");
+
   const client = buildClient(context);
   const { data } = await client.get("/api/users/currentuser");
   return data;
