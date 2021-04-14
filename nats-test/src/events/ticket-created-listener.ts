7dbc9848b7a520d4b8fffce96c6e6623d1e8b8f9
Shubham Gupta <shubhamg2595@gmail.com>
2021-04-14 23:56:58 +0530

    nats-test app typescrited for Listener validation

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,12 +1,16 @@
 import { Message } from "node-nats-streaming";
 
 import { Listener } from "./base-listener";
+import { Subjects } from "./subjects";
+import { TicketCreatedEvent } from "./ticket-created-events";
 
-export class TicketCreatedListener extends Listener {
-  subject = "ticket:created";
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  readonly subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = "payments-service";
-  onMessage(data: any, msg: Message): void {
+  onMessage(data: TicketCreatedEvent["data"], msg: Message): void {
     console.log("Event Data :::", data);
+
+    console.log(data.title);
 
     msg.ack();
   }
