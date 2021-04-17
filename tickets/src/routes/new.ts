fab518f43e4e3faa4ecde59b412bc77eb199e77c
Shubham Gupta <shubhamg2595@gmail.com>
2021-04-18 00:55:24 +0530

    shutdown, publish and listen tickets done

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,8 +2,8 @@ import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import { requireAuth, validateRequest } from "@msgtickets/common";
 import { Ticket } from "../models/ticket";
-import {TicketCreatedPublisher} from '../events/publishers/ticket-created-publisher'
-
+import { TicketCreatedPublisher } from "../events/publishers/ticket-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
 const router = express.Router();
 
 router.post(
@@ -27,12 +27,12 @@ router.post(
 
     await ticket.save();
 
-    // new TicketCreatedPublisher(client).publish({
-    //   id: ticket.id,
-    //   title: ticket.title,
-    //   price: ticket.price,
-    //   userId: ticket.userId,
-    // })
+    new TicketCreatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.status(201).send(ticket);
   }
