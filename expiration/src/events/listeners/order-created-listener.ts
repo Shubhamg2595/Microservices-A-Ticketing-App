1663262a80997748ce2a41d033d72fe4f054b6e3
Shubham Gupta <shubhamg2595@gmail.com>
2021-05-29 23:02:29 +0530

    expiration service setup done with listener and BullJS

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,20 @@
+import { Listener, OrderCreatedEvent, Subjects } from "@msgtickets/common";
+import { Message } from "node-nats-streaming";
+import { queueGroupName } from "./queueGroupName";
+import { expirationQueue } from "../../queues/expiration-queue";
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    await expirationQueue.add(
+      {
+        orderId: data.id,
+      },
+      {
+        delay: 10000,
+      }
+    );
+
+    msg.ack();
+  }
+}
