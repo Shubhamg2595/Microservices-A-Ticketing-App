40e8dbe61fa656b3d4f72fec42a92887ae7d2412
Shubham Gupta <shubhamg2595@gmail.com>
2021-05-31 00:00:23 +0530

    Payments service setup complete

---
+++ payments/src/app.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+import { json } from "body-parser";
+import { errorHandler, NotFoundError, currentUser } from "@msgtickets/common";
+
+const app = express();
+
+app.use(json());
+
+app.set("trust proxy", true); // since traffic is coming from proxy i.e ingress engine, we need to tell express to allow http-connections from proxies.
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test", //making sure only http requests are entertained when environment is not set to test mode
+  })
+);
+
+app.use(currentUser);
+
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
