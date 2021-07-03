7b708137b3eb78868f00bc0b1456e839ab157866
Shubham Gupta <shubhamg2595@gmail.com>
2021-07-03 22:28:29 +0530

    client : create_TIcket form handling done

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -1,15 +1,40 @@
+import { useState } from "react";
 const newTicket = () => {
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+
+  function onBlur(event) {
+    const value = parseFloat(price);
+    if (isNaN(value)) {
+      return;
+    }
+    setPrice(value.toFixed(2));
+  }
+
   return (
     <div>
       <h1>Create a Ticket </h1>
       <form>
         <div className="form-group">
           <label>Title</label>
-          <input className="form-control" />
+          <input
+            value={title}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+            className="form-control"
+          />
         </div>
         <div className="form-group">
           <label>Price</label>
-          <input className="form-control" />
+          <input
+            value={price}
+            onChange={(e) => {
+              setPrice(e.target.value);
+            }}
+            onBlur={onBlur}
+            className="form-control"
+          />
         </div>
         <button className="btn btn-primary"> Submit </button>
       </form>
